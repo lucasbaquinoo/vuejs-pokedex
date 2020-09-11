e1f56a011786e812beff06c983e2882460651a9d
lucasbaquino <lucasbaquino@icloud.com>
2020-09-10 22:05:49 -0300

    Initial commit

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="pokemon">
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="currentImg">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4"> {{ num }} - {{ name | upper }} </p>
+            <p class="subtitle is-6"> {{ pokemon.type }} </p>
+          </div>
+        </div>
+        <div class="content">
+          <button class="button is-medium is-fullwidth" @click="mudarSprite"> Mudar sprite </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  created: function() {
+    axios.get(this.url).then(response => {
+      this.pokemon.type = response.data.types[0].type.name;
+      this.pokemon.front = response.data.sprites.front_default;
+      this.pokemon.back = response.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
+    })
+  },
+  data() {
+    return {
+      isFront: true,
+      currentImg: '',
+      pokemon: {
+        type: '',
+        front: '',
+        back: '',
+      },
+    }
+  },
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  filters: {
+    upper: function(value) {
+      var newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
+    }
+  },
+  methods: {
+    mudarSprite: function() {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      } else {
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  #pokemon {
+    margin-top: 2%;
+  }
+</style>
